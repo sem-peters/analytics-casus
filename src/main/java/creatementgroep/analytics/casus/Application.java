666050b6dc5ef1d7bc258e2522e1b38af26da8d6
Sem <sem.peters@hotmail.com>
2022-03-21 11:28:34 +0100

    Initialized files

---
+++ src/main/java/creatementgroep/analytics/casus/Application.java
@@ -0,0 +1,13 @@
+package creatementgroep.analytics.casus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/creatementgroep/analytics/casus/ApplicationTests.java
@@ -0,0 +1,13 @@
+package creatementgroep.analytics.casus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
