d45ac263fbe00e6851d011f67f39f1748995e04d
Sem Peters <sem.peters@hotmail.com>
2022-03-24 15:39:38 +0100

    Finalized finishing touches for login page, ready for review.

--- src/main/java/creatementgroep/analytics/casus/security/WebSecurityConfig.java
+++ src/main/java/creatementgroep/analytics/casus/security/WebSecurityConfig.java
@@ -24,6 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .defaultSuccessUrl("/", true)
                 .permitAll()
                 .and()
                 .logout()
